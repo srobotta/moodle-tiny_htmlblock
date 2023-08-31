d5e992717bab12470542f8532d4cc1f126ad3338
Stephan Robotta <stephan.robotta@bfh.ch>
2023-08-31 21:11:56 +0200

    Set version number to 1.0 and maturity to RC

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_htmlblock';
-$plugin->release = '0.1';
-$plugin->version = 2023080900;
+$plugin->release = '1.0';
+$plugin->version = 2023083000;
 $plugin->requires = 2022112800;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_RC;
